fd6df802e31084225710e676d1b1fba96cfbabd0
trihd-3422 <ho.duc.tri@sun-asterisk.com>
2024-03-08 14:54:22 +0700

    commit Notifications Exercise

--- app/src/main/java/com/example/notificationsexercise/MainActivity.kt
+++ app/src/main/java/com/example/notificationsexercise/MainActivity.kt
@@ -1,16 +1,63 @@
 package com.example.notificationsexercise
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import com.example.notificationsexercise.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        binding.btnNotify.setOnClickListener {
+            createNotification(1)
+        }
+        binding.btnUpdate.setOnClickListener {
+            createNotification(System.currentTimeMillis().toInt())
+        }
+        binding.btnCancel.setOnClickListener {
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.cancelAll()
+        }
 
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun createNotificationChannel(){
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channel = NotificationChannel(CHANNEL_ID, "Test notification", NotificationManager.IMPORTANCE_HIGH)
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun createNotification(count: Int){
+        createNotificationChannel()
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this,0, intent, PendingIntent.FLAG_MUTABLE)
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("You have been notify!")
+            .setContentText("This is your notification text ---> $count")
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(1, notification)
+    }
+
+    companion object{
+        const val CHANNEL_ID = "CHANNEL_ID"
     }
 }
